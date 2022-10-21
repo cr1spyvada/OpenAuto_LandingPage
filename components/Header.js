993dee1ffc4d73602cf6e54dbd0614eced2759b6
cr1spyvada <vaisakhnp0700@gmail.com>
2022-10-21 23:19:10 +0530

    first page + header first iteration

--- components/Header.js
+++ components/Header.js
@@ -1,15 +1,27 @@
 import React from "react";
+import Image from "next/image";
 
 const Header = () => {
   return (
-    <div className="flex p-6">
+    <div className="flex">
       <div className="flex-[1] text-2xl font-semibold items-center">
         OPENAUTO
       </div>
       <div className="flex-[1] hidden text-lg md:flex justify-between items-center">
-        <div className="">+769 586 4558</div>
-        <div className="">service@openauto.ca</div>
-        <div className="border rounded-full p-2 px-6">
+        <div className="flex items-center">
+          <Image
+            src="/phoneicon.svg"
+            alt="Vercel Logo"
+            width={22}
+            height={18}
+          />
+          +769 586 4558
+        </div>
+        <div className="flex items-center">
+          <Image src="/mail.svg" alt="Vercel Logo" width={22} height={18} />
+          service@openauto.ca
+        </div>
+        <div className="border-2 border-gray-400 rounded-full p-2 px-6">
           Download the mobile app
         </div>
       </div>
--- components/HomePage.js
+++ components/HomePage.js
@@ -1,19 +1,58 @@
 import React from "react";
+import Image from "next/image";
 
 const HomePage = () => {
   return (
-    <div className="flex flex-col lg:flex-row">
+    <div className="h-[90%] flex flex-col lg:flex-row">
       {/* first col */}
-      <div className="flex flex-col">
-        {/* text on left */}
-        <div className="">
-          <div className="text-2xl font-bold">
+      <div className="h-[50%] lg:h-full lg:gap-y-10 lg:flex-[2] flex flex-col justify-center">
+        {/* text*/}
+        <div className="lg:flex flex-col gap-y-4">
+          <div className="text-center text-gray-300 lg:text-left text-2xl lg:text-5xl font-bold">
             Vehicle Maintenance From The Comfort of Your Home
           </div>
+          <div className="text-center lg:text-left px-10 lg:px-0 text-xs lg:text-sm">
+            Open Auto Soothes the hassle of maintaining your vehicle and helps
+            you deal with unexpected repairs worry-free.
+          </div>
         </div>
-        <div className=""></div>
+        <form className="w-full lg:w-3/5 space-y-2 lg:space-y-4">
+          <input
+            type="text"
+            className="border border-gray-500 bg-transparent p-2 px-4 w-full rounded-full"
+            placeholder="Enter Your Name"
+          />
+          <input
+            type="text"
+            className="border border-gray-500 bg-transparent p-2 px-4 w-full rounded-full"
+            placeholder="Enter Your Email"
+          />
+          <button className="border border-gray-500 font-medium p-2 w-full rounded-full text-center hover:bg-purple-500 hover:border-purple-500 hover:text-white">
+            Submit
+          </button>
+        </form>
       </div>
-      <div className=""></div>
+      <div className="h-[50%] lg:flex-[3] flex flex-col lg:h-full justify-between">
+        <div className="h-[75%] lg:h-[90%] items-center">
+          <img
+            src="/Pickup_Illustration.png"
+            style={{ height: "100%", margin: "auto" }}
+            alt="pickup image"
+          />
+        </div>
+        <div className="lg:ml-auto flex text-white h-5 justify-center gap-x-2">
+          <Image src="/facebook.svg" alt="Vercel Logo" width={32} height={16} />
+          <Image src="/twitter.svg" alt="Vercel Logo" width={32} height={16} />
+          <Image src="/youtube.svg" alt="Vercel Logo" width={32} height={16} />
+          <Image src="/linkedin.svg" alt="Vercel Logo" width={32} height={16} />
+          <Image
+            src="/instagram.svg"
+            alt="Vercel Logo"
+            width={32}
+            height={16}
+          />
+        </div>
+      </div>
     </div>
   );
 };
